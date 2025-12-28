a331fad9a71dc065704865d2ec2ff8441f4270a5
Bongo <bongopoyo@proton.me>
2025-12-28 14:12:04 +0500

    hehe

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
 use color_eyre::eyre::{Ok, Result};
 use ratatui::{
-    DefaultTerminal,
-    crossterm::{
-        event::{self, Event, KeyCode, KeyEvent},
-        terminal,
-    },
+    DefaultTerminal, Frame,
+    crossterm::event::{self, Event, KeyCode, KeyEvent},
+    widgets::{Paragraph, Widget},
 };
+
+#[derive(Debug, Default)]
+struct AppState {
+    items: Vec<TodoItem>,
+}
+#[derive(Debug, Default)]
+struct TodoItem {
+    is_done: bool,
+    description: String,
+}
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let mut state = AppState::default();
     color_eyre::install()?;
 
     let terminal = ratatui::init();
 
-    let result = run(terminal);
+    let result = run(terminal, &mut state);
     ratatui::restore();
     result
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     loop {
         // Rendering
+        terminal.draw(|f| render(f, app_state))?;
         // Input Handling
         if let Event::Key(key) = event::read()? {
             let to_exit = handle_key_presses(key);
@@ -36,4 +45,8 @@ fn handle_key_presses(key: KeyEvent) -> bool {
         event::KeyCode::Esc => true,
         _ => false,
     }
+}
+
+fn render(frame: &mut Frame, app_state: &mut AppState) {
+    Paragraph::new("Hello!").render(frame.area(), frame.buffer_mut());
 }
